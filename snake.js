0c5ba0bb5725d3bf3466ddb60ba6971c4976effd
partha <pparthapaul11@gmail.com>
2021-06-16 01:23:51 +0530

    gameover function added

--- snake.js
+++ snake.js
@@ -12,13 +12,16 @@ let snake = [
   { x: 10, y: 9 },
 ];
 
-let speed = 20;
+let speed = 10;
 let lastTime = 0;
 let foodLoaction = { x: 5, y: 15 };
+let pause = false;
+
 
 window.requestAnimationFrame(main);
 
 // functions
+// dont write code on main fucntion 
 
 function main(cTime) {
   window.requestAnimationFrame(main);
@@ -26,14 +29,50 @@ function main(cTime) {
     return 0;
   }
   lastTime = cTime;
-  // console.log(cTime);
+
+  board.innerHTML = "";
+
+  snakeMove();
+
+  foodUpdate();
+
+  gameover();
 
   engine();
 }
 
-function engine() {
-  board.innerHTML = "";
-  // changing the snake arrey
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      snakeDir = { x: 0, y: -1 };
+      pause = false;
+
+      break;
+    case "ArrowDown":
+      snakeDir = { x: 0, y: 1 };
+      pause = false;
+      break;
+    case "ArrowLeft":
+      snakeDir = { x: -1, y: 0 };
+      pause = false;
+      break;
+    case "ArrowRight":
+      snakeDir = { x: 1, y: 0 };
+      pause = false;
+      break;
+    case "Enter":
+      snakeDir = { x: 0, y: 0 };
+      pause = true;
+      break;
+
+    default:
+      break;
+  }
+});
+
+// moving the snake
+
+function snakeMove() {
   snake.forEach((e, index) => {
     // i know this part is really bad but at 2 am i cnat think something else
     if (e.x >= 20) e.x = e.x - 20;
@@ -44,8 +83,12 @@ function engine() {
     if (index != 0) {
       body.style.gridRowStart = e.y;
       body.style.gridColumnStart = e.x;
-      snake[snake.length - index].x = snake[snake.length - index - 1].x;
-      snake[snake.length - index].y = snake[snake.length - index - 1].y;
+
+      if (!pause) {
+        snake[snake.length - index].x = snake[snake.length - index - 1].x;
+        snake[snake.length - index].y = snake[snake.length - index - 1].y;
+      }
+
       let sbody = body.cloneNode(true);
       board.appendChild(sbody);
     } else {
@@ -57,43 +100,37 @@ function engine() {
 
     board.appendChild(head);
   });
+}
+// gameover function
 
-  // updating the food and snake arrey
-
+function foodUpdate() {
   if (foodLoaction.x == snake[0].x && foodLoaction.y == snake[0].y) {
     snake.unshift({ x: foodLoaction.x, y: foodLoaction.y });
 
-    foodLoaction = {
-      x: Math.round(Math.random() * 20),
-      y: Math.round(Math.random() * 20),
-    };
+    // set timeout to change the after a litte delay to make it real
+    setTimeout(() => {
+      foodLoaction = {
+        x: Math.round(Math.random() * 20),
+        y: Math.round(Math.random() * 20),
+      };
+    }, 50);
+
     console.log("food loacation change");
   }
   food.style.gridRowStart = foodLoaction.y;
   food.style.gridColumnStart = foodLoaction.x;
-  board.appendChild(food);
 
-  console.log(snake);
+  board.appendChild(food);
 }
 
-// arrow button events
-
-window.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "ArrowUp":
-      snakeDir = { x: 0, y: -1 };
-
-      break;
-    case "ArrowDown":
-      snakeDir = { x: 0, y: 1 };
-      break;
-    case "ArrowLeft":
-      snakeDir = { x: -1, y: 0 };
-      break;
-    case "ArrowRight":
-      snakeDir = { x: 1, y: 0 };
-      break;
-    default:
-      break;
+function gameover() {
+  // there is a bug i it should work from zero but it did not
+  for (let i = 3; i < snake.length; i++) {
+    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+      snakeDir = { x: 0, y: 0 };
+      pause = true;
+    }
   }
-});
+}
+
+
